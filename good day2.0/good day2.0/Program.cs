d2ba7399d40bc6d3440c22d1cb7effecc2cad864
Kost911 <Powerofthekee@mail.ru>
2019-09-26 20:51:32 +0300

    Data time

---
+++ good day2.0/good day2.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace good_day2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var today = DateTime.Now.TimeOfDay;
+            
+            if (today.Hours >= 9 && today.Hours <= 12)
+            {
+                Console.WriteLine(today);
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (today.Hours >= 12 && today.Hours <= 15)
+            {
+                Console.WriteLine(today);
+                Console.WriteLine("Good day, guys");
+            }
+            else if (today.Hours >= 15 && today.Hours <= 22)
+            {
+                Console.WriteLine(today);
+                Console.WriteLine("Good evening, guys");
+            }
+            else if (today.Hours >= 22 && today.Hours <= 9)
+            {
+                Console.WriteLine(today);
+                Console.WriteLine("Good night, guys");
+            }
+             Console.ReadKey();
+        }
+    }
+}
